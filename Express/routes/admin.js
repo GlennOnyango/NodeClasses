7dec4084add05893387e7b94f4dc886a425d4274
GlennOnyango <glenntedd@gmail.com>
2023-03-09 22:18:25 +0300

    used pug and went well

--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product",{docTitle:"Add Product",path:"admin"});
 });
 
 // /admin/add-product => POST
--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop',{prods:adminData.products,docTitle:"Shop"});
+  res.render('shop',{prods:adminData.products,docTitle:"Shop",path:"shop"});
 });
 
 module.exports = router;
